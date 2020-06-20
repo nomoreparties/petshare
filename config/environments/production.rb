bf0c81fc37e52b3c3a3fee3406c0203a624a243f
Matthew <matthewdominicchu@gmail.com>
2020-06-20 19:10:34 +0800

    sendgrid5 vers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -131,7 +131,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings =
   {
 
-    :address            => 'matthewdominicchu@gmail.com',
+    :address            => 'smtp.sendgrid.net',
     :port               => 587,
     :user_name          => ENV["SENDGRID_USERNAME"],
     :password           => ENV["SENDGRID_PASSWORD"]
